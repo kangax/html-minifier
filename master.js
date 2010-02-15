acd065a74c24b903fb1e3165faa144b8a77a401e
Juriy Zaytsev <kangax@gmail.com>
2010-02-15 09:03:35 -0500

    Add GA.

--- master.js
+++ master.js
@@ -61,3 +61,15 @@
   };
   
 })();
+
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-1128111-22']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+  var ga = document.createElement('script'); 
+  ga.type = 'text/javascript'; 
+  ga.async = true;
+  ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+  document.getElementsByTagName('head')[0].appendChild(ga);
+})();
