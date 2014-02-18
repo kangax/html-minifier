a52ee9ac66a80040515522854ef2658dcf3292b9
XhmikosR <xhmikosr@users.sourceforge.net>
2014-02-18 17:51:38 +0200

    Gruntfile.js: switch to 2 space indentation to match the rest of the codebase.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,80 +1,80 @@
 module.exports = function(grunt) {
-    'use strict';
+  'use strict';
 
-    grunt.initConfig({
+  grunt.initConfig({
 
-    pkg: grunt.file.readJSON('package.json'),
-    banner: '/*!\n' +
-            ' * HTMLMinifier v<%= pkg.version %> (<%= pkg.homepage %>)\n' +
-            ' * Copyright 2010-<%= grunt.template.today("yyyy") %> <%= pkg.author %>\n' +
-            ' * Licensed under <%= pkg.license.type %> (<%= pkg.license.url %>)\n' +
-            ' */\n',
+  pkg: grunt.file.readJSON('package.json'),
+  banner: '/*!\n' +
+          ' * HTMLMinifier v<%= pkg.version %> (<%= pkg.homepage %>)\n' +
+          ' * Copyright 2010-<%= grunt.template.today("yyyy") %> <%= pkg.author %>\n' +
+          ' * Licensed under <%= pkg.license.type %> (<%= pkg.license.url %>)\n' +
+          ' */\n',
 
-        jshint: {
-            options: {
-                jshintrc: '.jshintrc'
-            },
-            gruntfile: {
-                src: 'Gruntfile.js'
-            },
-            src: {
-                src: 'src/**/*.js'
-            },
-            tests: {
-                src: 'tests/*.js'
-            },
-            web: {
-                src: 'master.js'
-            }
-        },
+    jshint: {
+      options: {
+        jshintrc: '.jshintrc'
+      },
+      gruntfile: {
+        src: 'Gruntfile.js'
+      },
+      src: {
+        src: 'src/**/*.js'
+      },
+      tests: {
+        src: 'tests/*.js'
+      },
+      web: {
+        src: 'master.js'
+      }
+    },
 
-        concat: {
-            options: {
-                banner: '<%= banner %>'
-            },
-            dist: {
-                src: ['src/htmlparser.js',
-                      'src/htmlminifier.js',
-                      'src/htmllint.js'],
-                dest: 'dist/htmlminifier.js'
-            }
-        },
+    concat: {
+      options: {
+        banner: '<%= banner %>'
+      },
+      dist: {
+        src: ['src/htmlparser.js',
+              'src/htmlminifier.js',
+              'src/htmllint.js'],
+        dest: 'dist/htmlminifier.js'
+      }
+    },
 
-        uglify: {
-            options: {
-                banner: '<%= banner %>',
-                compress: true,
-                mangle: true,
-                preserveComments: false,
-                report: 'min'
-            },
-            minify: {
-                files: {
-                    'dist/htmlminifier.min.js': '<%= concat.dist.dest %>'
-                }
-            }
+    uglify: {
+      options: {
+        banner: '<%= banner %>',
+        compress: true,
+        mangle: true,
+        preserveComments: false,
+        report: 'min'
+      },
+      minify: {
+        files: {
+          'dist/htmlminifier.min.js': '<%= concat.dist.dest %>'
         }
+      }
+    }
 
 
-    });
+  });
 
-    require('load-grunt-tasks')(grunt, { scope: 'devDependencies' });
-    require('time-grunt')(grunt);
+  require('load-grunt-tasks')(grunt, { scope: 'devDependencies' });
+  require('time-grunt')(grunt);
 
-    grunt.registerTask('build', [
-        'concat'
-    ]);
+  grunt.registerTask('build', [
+    'concat'
+  ]);
 
-    grunt.registerTask('dist', [
-        'concat',
-        'uglify'
-    ]);
+  grunt.registerTask('dist', [
+    'concat',
+    'uglify'
+  ]);
 
-    grunt.registerTask('test', [
-        'concat',
-        'jshint'
-    ]);
+  grunt.registerTask('test', [
+    'concat',
+    'jshint'
+  ]);
 
-    grunt.registerTask('default', 'test');
+  grunt.registerTask('default', 'test');
 
 };
