ab53c89741a592bfa08fda956ad74d37a7a74ab7
XhmikosR <xhmikosr@users.sourceforge.net>
2014-03-09 09:49:20 +0200

    Enforce Unix line endings.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,9 @@
 module.exports = function(grunt) {
   'use strict';
 
+  // Force use of Unix newlines
+  grunt.util.linefeed = '\n';
+
   grunt.initConfig({
 
     pkg: grunt.file.readJSON('package.json'),
