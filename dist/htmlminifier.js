1f6af07f0d50fa8226bfd47f298ace39866aa44c
Laurent Goderre <laurent.goderre@gmail.com>
2014-09-04 13:29:13 -0400

    Added the preserveLineBreaks options to comments

--- dist/htmlminifier.js
+++ dist/htmlminifier.js
@@ -1180,6 +1180,9 @@
         currentChars = '';
       },
       chars: function( text, prevTag, nextTag ) {
+        prevTag = prevTag === '' ? 'comment' : prevTag;
+        nextTag = nextTag === '' ? 'comment' : nextTag;
+
         if (isIgnoring) {
           buffer.push(text);
           return;
@@ -1204,7 +1207,7 @@
         }
         if (options.collapseWhitespace) {
           if (!stackNoTrimWhitespace.length) {
-            text = (prevTag || nextTag) ?
+            text = ((prevTag && prevTag !== 'comment') || (nextTag && nextTag !== 'comment')) ?
               collapseWhitespaceSmart(text, prevTag, nextTag, options)
               : trimWhitespace(text);
           }
