ab64ca717238dbcf59b396f6a1fff1fd2515c8c9
XhmikosR <xhmikosr@gmail.com>
2014-12-23 20:04:32 +0200

    Make benchmark.js cross-platform.
    Also, check for failures in all places.

--- benchmark.js
+++ benchmark.js
@@ -1,6 +1,9 @@
+#!/usr/bin/env node
+
 /* jshint strict:false */
 
 var fs = require('fs'),
+    path = require('path'),
     exec = require('child_process').exec,
     Table = require('cli-table');
 
@@ -25,16 +28,16 @@ var table = new Table({
 
 console.log('');
 
+function toKb(size) {
+  return (size / 1024).toFixed(2);
+}
+
 function redSize(size) {
   return '\033[91m' + size + '\033[0m (' + toKb(size) + 'KB)';
 }
 
 function greenSize(size) {
   return '\033[92m' + size + '\033[0m (' + toKb(size) + 'KB)';
-}
-
-function toKb(size) {
-  return (size / 1024).toFixed(2);
 }
 
 function blueSavings(oldSize, newSize) {
@@ -56,24 +59,31 @@ function test(fileName, done) {
 
   console.log('Processing...', fileName);
 
-  var filePath = 'benchmarks/' + fileName + '.html';
-  var minifiedFilePath = 'benchmarks/generated/' + fileName + '.min.html';
-  var gzFilePath = 'benchmarks/generated/' + fileName + '.html.gz';
-  var gzMinifiedFilePath = 'benchmarks/generated/' + fileName + '.min.html.gz';
-  var command = './cli.js ' + filePath + ' -c benchmark.conf' + ' -o ' + minifiedFilePath;
+  var filePath = path.join('benchmarks/', fileName + '.html');
+  var minifiedFilePath = path.join('benchmarks/generated/', fileName + '.min.html');
+  var gzFilePath = path.join('benchmarks/generated/', fileName + '.html.gz');
+  var gzMinifiedFilePath = path.join('benchmarks/generated/', fileName + '.min.html.gz');
+  var command = path.normalize('./cli.js') + ' ' + filePath + ' -c benchmark.conf' + ' -o ' + minifiedFilePath;
 
   // Open and read the size of the original input
   fs.stat(filePath, function (err, stats) {
+    if (err) {
+      throw new Error('There was an error reading ' + filePath);
+    }
     var originalSize = stats.size;
 
     exec('gzip --keep --force --best --stdout ' + filePath + ' > ' + gzFilePath, function () {
       // Open and read the size of the gzipped original
       fs.stat(gzFilePath, function (err, stats) {
+        if (err) {
+          throw new Error('There was an error reading ' + gzFilePath);
+        }
+
         var gzOriginalSize = stats.size;
 
         // Begin timing after gzipped fixtures have been created
         var startTime = new Date();
-        exec(command, function () {
+        exec('node ' + command, function () {
 
           // Open and read the size of the minified output
           fs.stat(minifiedFilePath, function (err, stats) {
@@ -88,6 +98,10 @@ function test(fileName, done) {
             exec('gzip --keep --force --best --stdout ' + minifiedFilePath + ' > ' + gzMinifiedFilePath, function () {
               // Open and read the size of the minified+gzipped output
               fs.stat(gzMinifiedFilePath, function (err, stats) {
+                if (err) {
+                  throw new Error('There was an error reading ' + gzMinifiedFilePath);
+                }
+
                 var gzMinifiedSize = stats.size;
                 var gzMinifiedTime = new Date() - startTime;
 
