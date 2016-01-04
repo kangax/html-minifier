4ad7b714e4cf241d62330b92fce37723f83c3533
Juriy Zaytsev <kangax@gmail.com>
2016-01-04 14:08:11 -0800

    Revert "[In Progress] - SVG Minification using SVGO"

--- src/htmlminifier.js
+++ src/htmlminifier.js
@@ -561,34 +561,6 @@
     return text;
   }
 
-  function minifySVG(text, options) {
-    if (!text.trim().length) {
-      return text;
-    }
-
-    if (typeof options !== 'object') {
-      options = { };
-    }
-
-    try {
-      if (typeof require === 'function'){
-        var SVGO = require( 'svgo' );
-
-        if (text){
-          var svgo1 = new SVGO(options);
-          svgo1.optimize(text, function (rr) {
-            text = rr.data;
-          });
-        }
-      }
-    }
-    catch (err) {
-      log(err);
-    }
-
-    return text.trim();
-  }
-
   function minify(value, options) {
 
     options = options || {};
@@ -766,9 +738,6 @@
         }
         if (currentTag === 'style' && options.minifyCSS) {
           text = minifyCSS(text, options.minifyCSS);
-        }
-        if (currentTag === 'svg' && options.minifySVG) {
-          text = minifySVG(text, options.minifySVG);
         }
         if (options.collapseWhitespace) {
           if (!stackNoTrimWhitespace.length) {
