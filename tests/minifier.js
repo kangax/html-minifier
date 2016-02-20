a984a3bacd3089769d75b8b19efc9c26d296c047
alexlamsl <alexlamsl@gmail.com>
2016-02-21 06:25:49 +0800

    add more tests

--- tests/minifier.js
+++ tests/minifier.js
@@ -803,6 +803,11 @@
     equal(minify(input, { removeOptionalTags: true }), output);
     equal(minify(input, { removeOptionalTags: true, removeEmptyElements: true }), output);
 
+    input = '<html><head></head><body></body></html>';
+    output = '';
+    equal(minify(input, { removeOptionalTags: true }), output);
+    equal(minify(input, { removeOptionalTags: true, removeEmptyElements: true }), output);
+
     input = '<html><head><title>hello</title></head><body><p>foo<span>bar</span></p></body></html>';
     equal(minify(input), input);
     output = '<title>hello</title><p>foo<span>bar</span>';
