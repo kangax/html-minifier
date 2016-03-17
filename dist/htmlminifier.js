1ee87f539e36ae4a2945dcb8ec5a87407226b016
Josh Crowther <jshcrowthe@gmail.com>
2016-03-17 09:21:45 -0600

    Refactor use of [].indexOf() -> makeMap

--- dist/htmlminifier.js
+++ dist/htmlminifier.js
@@ -95,11 +95,8 @@
   var phrasingOnly = makeMap('abbr,b,bdi,bdo,button,cite,code,data,dfn,em,h1,h2,h3,h4,h5,h6,i,kbd,label,legend,mark,meter,output,p,pre,progress,q,rp,rt,s,samp,small,span,strong,sub,sup,time,u,var');
   var phrasing = makeMap('a,abbr,area,audio,b,bdi,bdo,br,button,canvas,cite,code,data,datalist,del,dfn,em,embed,i,iframe,img,input,ins,kbd,keygen,label,link,main,map,mark,math,menu,meter,nav,noscript,object,ol,output,p,picture,pre,progress,q,ruby,s,samp,script,section,select,small,span,strong,sub,sup,svg,table,template,textarea,time,u,ul,var,video,wbr');
 
-  var isKnownTag = function(tagName) {
-    // List comes from https://html.spec.whatwg.org/multipage/indices.html#elements-3
-    var knownTags = ["a","abbr","address","area","article","aside","audio","b","base","bdi","bdo","blockquote","body","br","button","canvas","caption","cite","code","col","colgroup","data","datalist","dd","del","details","dfn","dialog","div","dl","dt","em","embed","fieldset","figcaption","figure","footer","form","h1","head","header","hgroup","hr","html","i","iframe","img","input","ins","kbd","keygen","label","legend","li","link","main","map","mark","math","menu","menuitem","meta","meter","nav","noscript","object","ol","optgroup","option","output","p","param","picture","pre","progress","q","rp","rt","ruby","s","samp","script","section","select","small","source","source","span","strong","style","sub","summary","sup","svg","table","tbody","td","template","textarea","tfoot","th","thead","time","title","tr","track","u","ul","var","video","wbr"];
-    return knownTags.indexOf(tagName) > -1;
-  };
+  // List comes from https://html.spec.whatwg.org/multipage/indices.html#elements-3
+  var html5Tags = makeMap('a,abbr,address,area,article,aside,audio,b,base,bdi,bdo,blockquote,body,br,button,canvas,caption,cite,code,col,colgroup,data,datalist,dd,del,details,dfn,dialog,div,dl,dt,em,embed,fieldset,figcaption,figure,footer,form,h1,head,header,hgroup,hr,html,i,iframe,img,input,ins,kbd,keygen,label,legend,li,link,main,map,mark,math,menu,menuitem,meta,meter,nav,noscript,object,ol,optgroup,option,output,p,param,picture,pre,progress,q,rp,rt,ruby,s,samp,script,section,select,small,source,source,span,strong,style,sub,summary,sup,svg,table,tbody,td,template,textarea,tfoot,th,thead,time,title,tr,track,u,ul,var,video,wbr');
 
   var reCache = {};
 
@@ -285,7 +282,7 @@
       var tagName = match.tagName;
       var unarySlash = match.unarySlash;
 
-      if (handler.html5 && lastTag && phrasingOnly[lastTag] && !phrasing[tagName] && isKnownTag(tagName)) {
+      if (handler.html5 && lastTag && phrasingOnly[lastTag] && !phrasing[tagName] && html5Tags[tagName]) {
         parseEndTag( '', lastTag );
       }
 
