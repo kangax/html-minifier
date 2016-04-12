25bcdfec034f8a29c072e1bbcb2c913bc43a5c1b
alexlamsl <alexlamsl@gmail.com>
2016-04-12 19:59:36 +0800

    remove time-grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -68,7 +68,6 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-browserify');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-eslint');
-  require('time-grunt')(grunt);
 
   var phantomjs = require('grunt-lib-phantomjs').init(grunt);
   var webErrors;
