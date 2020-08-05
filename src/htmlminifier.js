6956de03e4ac68480c41cfe2674483543a164c6e
Doug Linning <62891957+dlinning-jockey@users.noreply.github.com>
2020-08-05 13:45:10 -0500

    Added a missing null check to collapseWhitespace
    This was breaking a script, as there was no null checking in place. `str.replace()` was the specific breaking point.

--- src/htmlminifier.js
+++ src/htmlminifier.js
@@ -21,6 +21,10 @@ function collapseWhitespaceAll(str) {
 
 function collapseWhitespace(str, options, trimLeft, trimRight, collapseAll) {
   var lineBreakBefore = '', lineBreakAfter = '';
+  
+  if(str == null){
+    return '';
+  }
 
   if (options.preserveLineBreaks) {
     str = str.replace(/^[ \n\r\t\f]*?[\n\r][ \n\r\t\f]*/, function() {
